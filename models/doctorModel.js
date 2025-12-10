fa7e6144d1a2396bf2a92a08a5377b0649d999f8
Luffycoder0 <luffycoding0@gmail.com>
2025-12-10 19:59:01 +0200

    Gender edit2

--- models/doctorModel.js
+++ models/doctorModel.js
@@ -33,7 +33,7 @@ const doctorSchema = new mongoose.Schema(
     },
     gender: {
       type: String,
-      enum: ['male', 'female'],
+      enum: ['Male', 'Female'],
       required: [true, 'Please specify your gender']
     },
     address: {
